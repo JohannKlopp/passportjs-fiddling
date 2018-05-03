5d8c223421e2a260993757f4de42df180b6f701f
Johann Klopp <clouduser@i-791.localdomain>
2018-05-03 15:01:14 +0100

    add secure-password storage, ensureAuthenticated feature, forgot-pw feature

--- myapp/app.js
+++ myapp/app.js
@@ -1,26 +1,34 @@
 //Basic packages
 const express = require("express");
+const flash = require("express-flash");
 const bodyParser = require("body-parser");
 const path = require("path");
 // const favicon = require("serve-favicon");
 const logger = require("morgan");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+
 //Security related packages
 const nodemailer = require("nodemailer");
 const passport = require("passport");
 const localStrategy = require("passport-local").Strategy;
-const bcrypt = require("bcrypt-nodejs");
-const async = require("async");
-const crypto = require("crypto");
+const securePassword = require("secure-password");
+
+//Local imports
+const {mongoose} = require("./db/mongoose");
+const {User} = require("./models/user");
 
 //local authentication via passportjs
-passport.use(new localStrategy((username, password, done) => {
-  User.findOne({username: username}, (err, userFromDb) => {
+passport.use(new localStrategy({
+  usernameField: "email"
+},
+(username, password, done) => {
+  User.findOne({email: username}, (err, userFromDb) => {
     if(err) return done(err);
-    if(!userFromDb) return done(null, false, {message: "Incorrect username."});
-    userFromDb.comparePassword(password, (err, isMatch) => {
-      if(isMatch) {
+    if(!userFromDb) return done(null, false, {message: "Incorrect email."});
+
+    userFromDb.verifyPassword(password, (err, result) => {
+      if(result === securePassword.VALID) {
         return done(null, userFromDb);
       }
       else {
@@ -41,10 +49,6 @@ passport.deserializeUser((id, done) => {
   });
 });
 
-//Local imports
-const {mongoose} = require("./db/mongoose");
-const {User} = require("./models/user");
-
 var app = express();
 
 //Middleware
@@ -56,6 +60,7 @@ app.use(bodyParser.json()); //only used for/applied to the request body (req.bod
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(session({ secret: "session secret key"}));
+app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
 // app.use(express.static(path.join(__dirname, "public")));
@@ -74,22 +79,167 @@ app.get("/login", (req, res) => {
   });
 });
 
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if(err) return next(err)
-    if(!user) {
-      return res.redirect("/login")
-    }
-    req.logIn(user, (err) => {
-      if(err) return next(err);
-      return res.redirect("/");
-    });
-  })(req, res, next);
+app.post("/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
 });
+
+function ensureAuthenticated(req, res, next) {
+  if(req.isAuthenticated()) { return next() };
+  res.redirect("/login");
+};
+
+app.get("/secret", ensureAuthenticated, (req, res) => {
+  res.send("yippii");
+});
+
 
 app.get("/signup", (req, res) => {
   res.render("signup", {
     user: req.user
+  });
+});
+
+app.post("/signup", (req, res) => {
+  var newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  });
+  newUser.save((err) => {
+    if(err) console.error(err);
+    req.logIn(newUser, (err) => {
+      if(err) console.error(err);
+      res.redirect("/");
+    });
+  });
+});
+
+app.get("/forgot", (req, res) => {
+  res.render("forgot", {
+    user: req.user
+  });
+});
+
+// POST /forgot
+app.post('/forgot', function(req, res, next) {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        host:"smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "z4zq6z3elu5ame2h@ethereal.email",
+          pass: "APCxZCkJsCJVMVMRSF"
+        }
+      });
+      var mailOptions = {
+        from: 'passwordreset@demo.com',
+        to: user.email,
+        subject: 'Node.js Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'The following link is valid for 1 hour only. Please click on it, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+});
+
+app.get("/reset/:token", (req, res) => {
+  User.findOne({resetPasswordToken: req.params.token, resetPasswordExpires: {$gt: Date.now()}}, (err, user) => {
+    if(!user) {
+      req.flash("error", "Password reset token is invalid or has expired:");
+      return res.redirect("/forgot");
+    }
+    res.render("reset", {
+      user: req.user
+    });
+  });
+});
+
+// POST /reset/:token
+app.post('/reset/:token', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+
+        user.save(function(err) {
+          req.logIn(user, function(err) {
+            done(err, user);
+          });
+        });
+      });
+    },
+    function(user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        host:"smtp.ethereal.email",
+        port: 587,
+        secure: false,
+        auth: {
+          user: "z4zq6z3elu5ame2h@ethereal.email",
+          pass: "APCxZCkJsCJVMVMRSF"
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@demo.com',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+  ], function(err) {
+    res.redirect('/');
   });
 });
 
--- myapp/models/user.js
+++ myapp/models/user.js
@@ -1,11 +1,13 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt-nodejs");
+const securePassword = require("secure-password");
 
 var UserSchema = new mongoose.Schema({
   username: { //username is a "key", A key defines a property in a mongodb document
     type: String,
     required: true,
-    unique: true
+    unique: true,
+    minlength: 1
   },
   email: {
     type: String,
@@ -24,35 +26,71 @@ var UserSchema = new mongoose.Schema({
 });
 
 // On Sign up, check if "password" and "confirm password" are equal
+// UserSchema.pre("save", function (next) {
+//
+//   if(req.body.confirm === req.body.password) {
+//
+//   }
+// });
+
+// hash and salt user's password that was just entered in the form, prior to saving it to the db
+// UserSchema.pre("save", function (next) {
+//   var user = this; // In the .pre hook, "this" is the document that is about to be saved
+//   var SALT_FACTOR = 5;
+//   console.log(user);
+//   if(!user.isModified("password")) return next();
+//
+//   bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+//     if(err) return next(err);
+//     console.log(user.password);
+//     bcrypt.hash(user.password, salt, null, (err, hash) => {
+//       if(err) return next(err);
+//       user.password = hash;
+//       console.log(hash);
+//       next();
+//     });
+//   });
+// });
+
+var spw = securePassword();
+
 UserSchema.pre("save", function (next) {
+  var user = this;
+  var userPassword = Buffer.from(user.password);
 
-  if(req.body.confirm === req.body.password) {
-
-  }
-});
-
-// hash and salt user's password (s)he just entered in the form, prior to saving it to the db
-UserSchema.pre("save", function (next) {
-  var user = this; // In the .pre hook, "this" is the document that is about to be saved
-  var SALT_FACTOR = 5;
-
-  if(!user.isModified("password")) return next();
-
-  bcrypt.genSalt(SALT_FACTOR, function (err, salt) {
+  spw.hash(userPassword, (err, hash) => {
     if(err) return next(err);
-    bcrypt.hash(user.password, salt, null, (err, hash) => {
-      if(err) return next(err);
-      user.password = hash;
-      next();
-    });
+    user.password = hash;
+    next();
   });
 });
 
-// password verification when user tries to sign in
-UserSchema.methods.comparePassword = function (candidatePassword, callback) {
-  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+// password verification when user tries to log in
+// UserSchema.methods.comparePassword = function (candidatePassword, callback) {
+//   bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+//     if(err) return callback(err);
+//     callback(null, isMatch);
+//   });
+// };
+
+UserSchema.methods.verifyPassword = function (candidatePassword, callback) {
+  var bufcandidatePassword = Buffer.from(candidatePassword);
+  var bufExistingHash = Buffer.from(this.password);
+
+  spw.verify(bufcandidatePassword, bufExistingHash, (err, result) => {
     if(err) return callback(err);
-    callback(null, isMatch);
+
+    if(result === securePassword.INVALID_UNRECOGNIZED_HASH) console.error("Invalid and unrecognized hash for this password.");
+    if(result === securePassword.INVALID) console.error("Attempted auth with invalid hash. (incorrect password)");
+    // if(result === securePassword.VALID_NEEDS_REHASH) {
+    //   console.error("Valid hash, but rehashing to improve security.");
+    //   spw.hash(bufcandidatePassword, (err, improvedHash) => {
+    //     if(err) console.error("Authenticated, but rehash unsuccessful this time around.");
+    //     return user.password = improvedHash;
+    //   });
+    // };
+
+    callback(null, result);
   });
 };
 
